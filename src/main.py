f788449373314e04926d355d10782804812255aa
chickendining <sumethsiripakorn@gmail.com>
2023-12-25 22:15:17 +0700

    add main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
-import subprocess
+# import subprocess
 
-# Function to execute a shell command and return its output
-def run_shell_command(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    output, error = process.communicate()
-    return output, error
+# # Function to execute a shell command and return its output
+# def run_shell_command(command):
+#     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+#     output, error = process.communicate()
+#     return output, error
 
-# Inside main.py
-# List current directory contents
-output, error = run_shell_command("ls -a")
-if error:
-    print("Error:", error.decode())
-else:
-    print("Current directory contents:\n", output.decode())
+# # Inside main.py
+# # List current directory contents
+# output, error = run_shell_command("ls -a")
+# if error:
+#     print("Error:", error.decode())
+# else:
+#     print("Current directory contents:\n", output.decode())
 
-# Then, read the file
+# # Then, read the file
 try:
     with open("../caller_repo/config/org.txt", 'r') as file:
         content = file.read()
